28d88bfe3a374dbd8d680f5b0f239c51bbdcd325
wahid30 <md.wahidulalamjp2@gmail.com>
2022-11-24 09:22:13 +0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <button className="btn">Button</button>
-      <button className="btn btn-primary">Button</button>
-      <button className="btn btn-secondary">Button</button>
-      <button className="btn btn-accent">Button</button>
-      <button className="btn btn-ghost">Button</button>
-      <button className="btn btn-link">Button</button>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
