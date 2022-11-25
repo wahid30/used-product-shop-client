702acc1db7dae46f05d6845c306b32d0dc49b4c2
wahid30 <md.wahidulalamjp2@gmail.com>
2022-11-26 01:08:53 +0600

    implement input in modal

--- src/Pages/Home/MobileCategories/DisplayMobiles/BookingModal/BookingModal.js
+++ src/Pages/Home/MobileCategories/DisplayMobiles/BookingModal/BookingModal.js
@@ -1,8 +1,28 @@
 import React from "react";
 
 const BookingModal = ({ item }) => {
-  const { name } = item;
+  const {
+    name,
+    category_id,
+    details,
+    image_url,
+    location,
+    original_price,
+    rating,
+    resale_price,
+    yearsofUse,
+  } = item;
   // console.log(item);
+
+  const handleBooking = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const date = form.date.value;
+    const name = form.name.value;
+    const email = form.email.value;
+    const phone = form.phone.value;
+    console.log(date, name, email, phone);
+  };
   return (
     <>
       <input type="checkbox" id="booking-modal" className="modal-toggle" />
@@ -15,10 +35,47 @@ const BookingModal = ({ item }) => {
             ✕
           </label>
           <h3 className="text-lg font-bold">{name}</h3>
-          <p className="py-4">
-            You've been selected for a chance to get one year of subscription to
-            use Wikipedia for free!
-          </p>
+          <form
+            onSubmit={handleBooking}
+            className="mt-10 grid grid-cols-1 gap-3"
+          >
+            <input
+              name="date"
+              type="text"
+              placeholder="Enter Date"
+              className="input w-full input-bordered"
+            />
+            <input
+              type="text"
+              value={`Price: ${resale_price}$`}
+              className="input w-full input-bordered"
+              disabled
+            />
+            <input
+              name="name"
+              type="text"
+              placeholder="Your Name"
+              className="input w-full input-bordered"
+            />
+            <input
+              name="email"
+              type="email"
+              placeholder="Your Email"
+              className="input w-full input-bordered"
+            />
+            <input
+              name="phone"
+              type="text"
+              placeholder="Phone Number"
+              className="input w-full input-bordered"
+            />
+            <br />
+            <input
+              className="btn btn-success w-full "
+              type="submit"
+              value="Submit"
+            />
+          </form>
         </div>
       </div>
     </>
