4627381448c35db3af0db1c068c404d7371bbf16
wahid30 <md.wahidulalamjp2@gmail.com>
2022-11-28 21:26:46 +0600

    load booking data

--- src/Pages/Home/MobileCategories/DisplayMobiles/BookingModal/BookingModal.js
+++ src/Pages/Home/MobileCategories/DisplayMobiles/BookingModal/BookingModal.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useContext } from "react";
+import toast from "react-hot-toast";
+import { AuthContext } from "../../../../../contexts/AuthProvider";
 
 const BookingModal = ({ item, setItem }) => {
   const {
@@ -13,6 +15,7 @@ const BookingModal = ({ item, setItem }) => {
     yearsofUse,
   } = item;
   // console.log(item);
+  const { user } = useContext(AuthContext);
 
   const handleBooking = (event) => {
     event.preventDefault();
@@ -31,8 +34,27 @@ const BookingModal = ({ item, setItem }) => {
       phone,
       meetingLocation,
     };
-    console.log(booking);
-    setItem(null);
+
+    // console.log(booking);
+
+    fetch("http://localhost:5000/bookings", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(booking),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.acknowledged) {
+          setItem(null);
+          toast.success("Booking confirmed");
+          // refetch();
+        } else {
+          toast.error(data.message);
+        }
+      });
   };
   return (
     <>
@@ -65,12 +87,16 @@ const BookingModal = ({ item, setItem }) => {
             <input
               name="name"
               type="text"
+              defaultValue={user?.displayName}
+              disabled
               placeholder="Your Name"
               className="input w-full input-bordered"
             />
             <input
               name="email"
               type="email"
+              defaultValue={user?.email}
+              disabled
               placeholder="Your Email"
               className="input w-full input-bordered"
             />
--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
 import { useForm } from "react-hook-form";
 import toast from "react-hot-toast";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthProvider";
 
 const SignUp = () => {
@@ -12,6 +12,8 @@ const SignUp = () => {
   } = useForm();
   const { createUser, updateUser } = useContext(AuthContext);
   const [signUpError, setSignUPError] = useState("");
+  const navigate = useNavigate();
+
   const handleSignUp = (data) => {
     console.log(data);
     setSignUPError("");
@@ -24,7 +26,9 @@ const SignUp = () => {
           displayName: data.name,
         };
         updateUser(userInfo)
-          .then(() => {})
+          .then(() => {
+            navigate("/");
+          })
           .catch((err) => console.log(err));
       })
       .catch((error) => {
